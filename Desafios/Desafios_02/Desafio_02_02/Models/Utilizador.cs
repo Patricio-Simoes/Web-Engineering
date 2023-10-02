e0457d55ee90a8d7a2505f30fb354e7743c23051
patricio-simoes <code.psimoes@proton.me>
2023-10-02 15:06:45 +0100

    Desafio_02_02

---
+++ Desafios/Desafios_02/Desafio_02_02/Models/Utilizador.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace Desafio_02__2.Models
+{
+    public class Utilizador
+    {
+        [Required(ErrorMessage = "ERRO :: Este campo é necessário!")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "ERRO :: Este campo é necessário!")]
+        [RegularExpression(@"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$",
+        ErrorMessage = "ERRO :: Este endereço não é válido!")]
+        public string email { get; set; }
+        public bool terms { get; set; }
+    }
+}
