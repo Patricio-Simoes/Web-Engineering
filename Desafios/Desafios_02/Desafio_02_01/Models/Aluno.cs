2325c09be3f266f312027e6533cec81e185eeb13
patricio-simoes <code.psimoes@proton.me>
2023-10-20 18:56:10 +0100

    Updated Desafio_02_01

--- Desafios/Desafios_02/Desafio_02_01/Models/Aluno.cs
+++ Desafios/Desafios_02/Desafio_02_01/Models/Aluno.cs
@@ -5,10 +5,10 @@ namespace Desafio_02_01.Models
     public class Aluno
     {
         [Required(ErrorMessage = "ERRO :: Insira um número válido!")]
-        [RegularExpression("[1-9][1-9]*", ErrorMessage = "ERRO :: Apenas valores numéricos são válidos!")]
+        [RegularExpression("[1-9][0-9]*", ErrorMessage = "ERRO :: Apenas valores numéricos são válidos!")]
         public string numero { get; set; }
         [Required(ErrorMessage = "ERRO :: Insira um nome válido")]
-        [RegularExpression("[a-zA-Z][a-zA-Z]*", ErrorMessage = "ERRO :: Valores numéricos não são válidos!")]
+        [RegularExpression(@"^[a-zA-ZÀ-ÿ\s]+$", ErrorMessage = "ERRO :: Valores numéricos não são válidos!")]
         public string nome { get; set; }
     }
 }
