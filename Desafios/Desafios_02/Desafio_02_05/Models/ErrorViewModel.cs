32951b48cc9307c36540cf91db2c5f3b57dca88f
patricio-simoes <code.psimoes@proton.me>
2023-10-21 21:26:55 +0100

    Desafio_02_05

---
+++ Desafios/Desafios_02/Desafio_02_05/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Desafio_02_05.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
