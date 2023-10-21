17038dd31b904fda09822e5976650b85bfc8c2a0
patricio-simoes <code.psimoes@proton.me>
2023-10-21 20:22:29 +0100

    Desafio_02_03

---
+++ Desafios/Desafios_02/Desafio_02_03/Controllers/PessoaController.cs
@@ -0,0 +1,30 @@
+﻿using Desafio_02_03.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data;
+
+namespace Desafio_02_03.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public List<Pessoa> pessoas = new List<Pessoa>();
+        public ActionResult Index()
+        {
+            pessoas = GetPeopleFromDataSource();
+            return View(pessoas);
+        }
+
+        private List<Pessoa> GetPeopleFromDataSource()
+        {
+            var people = new List<Pessoa>
+            {
+                new Pessoa(1, "John Doe", 30, "123 Main St", "New York", "10001" ),
+                new Pessoa (2, "Jane Smith", 25, "456 Elm St", "Los Angeles", "90001"),
+        };
+
+            return people;
+        }
+
+    }
+}
---
+++ Desafios/Desafios_02/Desafio_02_03/Models/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Razor.Language.Extensions;
+
+namespace Desafio_02_03.Models
+{
+    public class Pessoa
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int idade { get; set; }
+        public string morada { get; set; }
+        public string cidade { get; set; }
+        public string postal { get; set; }
+
+        public Pessoa(int id, string nome, int idade, string morada, string cidade, string postal)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.idade = idade;
+            this.morada = morada;
+            this.cidade = cidade;
+            this.postal = postal;
+        }
+    }
+}
