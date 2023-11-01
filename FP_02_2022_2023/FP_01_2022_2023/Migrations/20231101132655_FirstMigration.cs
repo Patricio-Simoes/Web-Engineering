d5b86cfcadf5348e7313d11b504fca781fbcf550
patricio-simoes <code.psimoes@proton.me>
2023-11-01 14:48:02 +0000

    FP_02_2022_2023

---
+++ FP_02_2022_2023/FP_01_2022_2023/Migrations/20231101132655_FirstMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FP_01_2022_2023.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aluno",
+                columns: table => new
+                {
+                    numero = table.Column<int>(type: "int", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aluno", x => x.numero);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aluno");
+        }
+    }
+}
---
+++ FP_02_2022_2023/FP_01_2022_2023/Models/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FP_01_2022_2023.Models
+{
+    public class Aluno
+    {
+        [Key]
+        [Required(ErrorMessage = "Número Obrigatório")]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [DisplayName("Número")]
+        public int numero {  get; set; }
+        [Required(ErrorMessage = "E-mail Obrigatório")]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("E-mail")]
+        public string? email { get; set; }
+        [Required(ErrorMessage = "Nome Obrigatório")]
+        [DisplayName("Nome")]
+        public string? nome { get; set; }
+        [Required(ErrorMessage = "Data de Nascimento Obrigatória")]
+        [DataType(DataType.Date)]
+        [DisplayName("Idade")]
+        public DateTime date { get; set; }
+    }
+}
