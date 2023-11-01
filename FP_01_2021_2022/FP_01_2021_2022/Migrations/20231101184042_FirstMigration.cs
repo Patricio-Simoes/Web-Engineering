c78670d7977d1439c19b88fb705033b64ae68b5c
patricio-simoes <code.psimoes@proton.me>
2023-11-01 19:53:58 +0000

    FP_01_2021_2022

---
+++ FP_01_2021_2022/FP_01_2021_2022/Migrations/20231101184042_FirstMigration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FP_01_2021_2022.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacto",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    nickname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    amigo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacto", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacto");
+        }
+    }
+}
---
+++ FP_01_2021_2022/FP_01_2021_2022/Models/Contacto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FP_01_2021_2022.Models
+{
+    public class Contacto
+    {
+        [Key]
+        [Required]
+        public int id { get; set; }
+        [Required(ErrorMessage = "E-mail obrigatório")]
+        [DisplayName("E-mail")]
+        public string? email { get; set; }
+        [Required(ErrorMessage = "Nome obrigatório")]
+        [DisplayName("Nome")]
+        public string? name { get; set; }
+        [Required(ErrorMessage = "Nickname obrigatório")]
+        [MinLength(3, ErrorMessage = "Tamanho mínimo: {0}")]
+        [RegularExpression("^[a-zA-ZÀ-ÖØ-öø-ÿ]+$", ErrorMessage = "Apenas uma palavra")]
+        [DisplayName("Nickname")]
+        public string? nickname { get; set; }
+        [DisplayName("Amigo")]
+        public bool amigo { get; set; } = false;
+    }
+}
